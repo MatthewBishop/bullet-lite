5483b7b45c8ae5a4629b6ee5227d91f9bcba71df
MatthewBishop <55411296+MatthewBishop@users.noreply.github.com>
2022-09-08 15:56:37 -0400

    Create btBulletWicked.h

---
+++ src/btBulletWicked.h
@@ -0,0 +1,34 @@
+/*
+All Bullet includes used by wiPhysics_Bullet.cpp
+*/
+
+#ifndef BULLET_WICKED_H
+#define BULLET_WICKED_H
+
+///Common includes for Bullet Collision Detection
+#include "BulletDynamics/Dynamics/btRigidBody.h"
+#include "BulletDynamics/ConstraintSolver/btSequentialImpulseConstraintSolver.h"
+
+///Bullet's btCollisionObject definitions
+#include "BulletCollision/CollisionDispatch/btCollisionObject.h"
+
+///Collision Shapes
+#include "BulletCollision/CollisionShapes/btBoxShape.h"
+#include "BulletCollision/CollisionShapes/btSphereShape.h"
+#include "BulletCollision/CollisionShapes/btCapsuleShape.h"
+#include "BulletCollision/CollisionShapes/btConvexHullShape.h"
+#include "BulletCollision/CollisionShapes/btBvhTriangleMeshShape.h"
+#include "BulletCollision/CollisionShapes/btTriangleIndexVertexArray.h"
+
+///Dispatching and generation of collision pairs (broadphase)
+#include "BulletCollision/CollisionDispatch/btCollisionDispatcher.h"
+#include "BulletCollision/BroadphaseCollision/btDbvtBroadphase.h"
+
+///Math library & Utils
+#include "LinearMath/btQuaternion.h"
+#include "LinearMath/btTransform.h"
+#include "LinearMath/btDefaultMotionState.h"
+#include "LinearMath/btIDebugDraw.h"
+#include "LinearMath/btSerializer.h"
+
+#endif  //BULLET_WICKED_H
